24c1956d007a9a7e8efb174e6ce6a24c4eebb54c
e1nsim <sime867@newschool.edu>
2023-03-28 18:51:40 -0400

    json update

--- webtype-intro/projects/project-2/assets/script.js
+++ webtype-intro/projects/project-2/assets/script.js
@@ -17,7 +17,7 @@ let container = document.getElementById("container");
 // must setup a local server to use fetch
 // see Python instructions here:
 // https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server#using_python
-fetch('/assets/data.json')
+fetch('../assets/data.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
