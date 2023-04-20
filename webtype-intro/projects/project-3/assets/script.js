36a858c6cac33d85b0db59cfd4197bb0bdb2503c
e1nsim <sime867@newschool.edu>
2023-04-20 01:27:13 -0400

    updt

---
+++ webtype-intro/projects/project-3/assets/script.js
@@ -0,0 +1,28 @@
+const appToken = "9iD0u2SchkncVrxQ7Z33SPDf4";
+const apiUrl = "https://data.cityofnewyork.us/resource/hjae-yuav.json";
+
+fetch(`${apiUrl}?$$app_token=${appToken}`)
+  .then(response => response.json())
+  .then(data => {
+    const table = document.createElement("table");
+
+    const headerRow = table.insertRow();
+    Object.keys(data[0]).forEach(key => {
+      const cell = headerRow.insertCell();
+      cell.textContent = key;
+    });
+    const accessibleCell = headerRow.insertCell();
+    accessibleCell.textContent = "Handicap Accessible";
+
+
+    data.forEach(obj => {
+      const row = table.insertRow();
+      Object.keys(obj).forEach(key => {
+        const cell = row.insertCell();
+        cell.textContent = obj[key];
+      });
+    });
+
+    document.body.appendChild(table);
+  })
+  .catch(error => console.error(error));
